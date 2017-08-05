10f377f781633f5c9d8b9092252bfaae92eaae10
Torrey Rice <rice@sitepen.com>
2017-08-05 12:17:38 -0700

    removing deps we don’t need
    Signed-off-by: Torrey Rice <rice@sitepen.com>

--- site/themes/tutelage/scripts/fancybox.js
+++ site/themes/tutelage/scripts/fancybox.js
@@ -1,11 +1,11 @@
-var rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/;
+//var rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/;
 
 /**
 * Fancybox tag
 *
 * Syntax:
 *   {% fancybox /path/to/image [/path/to/thumbnail] [title] %}
-*/
+
 
 hexo.extend.tag.register('fancybox', function(args){
   var original = args.shift(),
@@ -22,3 +22,4 @@ hexo.extend.tag.register('fancybox', function(args){
     '</a>' +
     (title ? '<span class="caption">' + title + '</span>' : '');
 });
+  */
