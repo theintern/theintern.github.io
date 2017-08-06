d456a07300c2e5a4317792fdb8f7363647732fd6
Torrey Rice <rice@sitepen.com>
2017-08-06 12:06:13 -0700

    Simplifying doc fragments rendering

--- site/themes/tutelage/scripts/docs_parsing.js
+++ site/themes/tutelage/scripts/docs_parsing.js
@@ -1,15 +1,7 @@
-/*hexo.extend.processor.register('docs/*.md', function(file){
-    console.log(file);
-});*/
-var MarkdownIt = require('markdown-it'),
-md = new MarkdownIt();
-
-hexo.extend.filter.register('before_post_render', function(data){
-    
+hexo.extend.filter.register('before_post_render', function(data){    
   if(data.path.includes("docs/")) {
       if(data.source.includes(".md")) {
-        data.layout = 'false'; 
-       data.content = md.render(data._content);
+        data.layout = 'fragment'; 
       }
   }
   return data;
