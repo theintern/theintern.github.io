fb24da623dd6177f85e47fee7368f66d1dc366d7
Torrey Rice <rice@sitepen.com>
2017-08-13 16:59:29 -0700

    Going Metalsmith + Sass
    Signed-off-by: Torrey Rice <rice@sitepen.com>

--- build.js
+++ build.js
@@ -1,7 +1,12 @@
-var metalsmith = require('metalsmith');
-var markdown = require('metalsmith-markdownit');
-var layouts = require('metalsmith-layouts');
-var handlebars = require('handlebars');
+const metalsmith = require('metalsmith');
+const markdown = require('metalsmith-markdownit');
+const layouts = require('metalsmith-layouts');
+const ejs = require('ejs');
+const sass = require('metalsmith-sass');
+const watch = require('metalsmith-watch');
+const serve = require('metalsmith-serve');
+const assets = require('metalsmith-assets');
+const inlineSource = require('metalsmith-inline-source');
 
 metalsmith(__dirname)
 	.metadata({
@@ -12,14 +17,38 @@ metalsmith(__dirname)
 	})
 	.source('./src')
 	.destination('./public')
+	.use(markdown())
 	.use(layouts({
-		engine: 'handlebars',
+		engine: 'ejs',
 		directory: './resources/layouts',
-		default: 'default.hbs',
+		default: 'default.ejs',
 		pattern: "**/*.html",
 		partials: "./resources/layouts/partials"
 	}))
-	.use(markdown())
+	.use(sass({
+		outputStyle: "compressed",
+		sourceMap: true,
+		sourceMapContents: true
+	}))
+	.use(inlineSource({
+		rootpath: './src/'
+	}))
+	.use(assets({
+		source: './resources/assets',
+		destination: './resources/assets'
+	}))
+	.use(serve({
+		port: 4000,
+		verbose: true
+	}))
+	.use(watch({
+			paths: {
+				"${source}/**/*": true,
+				"layouts/**/*": "**/*.ejs",
+			},
+			livereload: true,
+		})
+	)
 	.build(function (err) {
 		if (err) {
 			console.log(err);
