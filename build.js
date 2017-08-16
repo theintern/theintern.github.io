beb22e02430f0f8e53d394f31b3d4436e59830ee
Jason Cheatham <j.cheatham@gmail.com>
2017-08-16 15:50:17 -0400

    Add publish capability to build script

--- build.js
+++ build.js
@@ -6,7 +6,9 @@ const browserSync = require('metalsmith-browser-sync');
 const inlineSource = require('metalsmith-inline-source');
 const ts = require('typescript');
 const uglifyJs = require('uglify-js');
+const { execSync } = require('child_process');
 
+let publish = false;
 let serve = false;
 let production = false;
 
@@ -19,8 +21,27 @@ process.argv.slice(2).forEach(arg => {
 		console.log('Building for production');
 		production = true;
 		break;
+	case 'publish':
+		publish = true;
+		production = true;
+		break;
+	case 'help':
+	case '-h':
+	case '--help':
+		printUsage();
+		process.exit(0);
+		break;
+	default:
+		console.error(`Unknown option "${arg}"\n`);
+		printUsage();
+		process.exit(1);
 	}
 });
+
+if (serve && publish) {
+	console.error('Only one of "serve" and "publish" may be specified');
+	process.exit(1);
+}
 
 const metalsmith = new Metalsmith(__dirname)
 	.metadata({
@@ -64,7 +85,7 @@ const metalsmith = new Metalsmith(__dirname)
 	);
 
 if (production) {
-	metalsmith.use(uglify())
+	metalsmith.use(uglify());
 }
 
 if (serve) {
@@ -78,11 +99,24 @@ if (serve) {
 	);
 }
 
+if (publish) {
+	console.log('Creating a clone of the master branch');
+	execSync('git clone -q . public');
+	execSync('git checkout -q master', { cwd: 'public' });
+}
+
 metalsmith.build(function(error) {
 	if (error) {
 		console.log(error);
 	} else {
 		console.log('Built!');
+
+		if (publish) {
+			execSync('git add .', { cwd: 'public' });
+			execSync('git commit --all -m "Updated doc build"', { cwd: 'public' });
+			execSync('git fetch public master:master'); 
+			console.log('Publishing');
+		}
 	}
 });
 
@@ -142,4 +176,11 @@ function uglify() {
 		});
 		done();
 	};
+}
+
+function printUsage() {
+	console.log('usage: build [serve | publish] [production]');
+	console.log('  serve      - start a dev server');
+	console.log('  publish    - build and publish the site');
+	console.log('  production - build in production mode');
 }
